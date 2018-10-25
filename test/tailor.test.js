01ad897e1a044c16472748d17299c45e3be51e96
Kyle Ladd <kladd6@gmail.com>
2018-10-24 21:07:44 -0400

    Test concat spaces in path by comparing input durations total output duration

--- test/tailor.test.js
+++ test/tailor.test.js
@@ -5,6 +5,7 @@ let test = require('tape');
 let path = require('path');
 let videoStitch = require('../index');
 let util = require('util');
+let shelljs = require('shelljs');
 
 
 test('Video Stitch Module', (t) => {
@@ -36,12 +37,12 @@ test('Video Stitch Module', (t) => {
     .catch(err => {
       t.fail(util.inspect(err));
     });
-})
+});
 
 
 test('Video Stitch Concat Module', (t) => {
   let videoConcat = videoStitch.concat;
-  t.plan(1);
+  t.plan(2);
   videoConcat({
     silent: false,
     overwrite: true
@@ -57,10 +58,42 @@ test('Video Stitch Concat Module', (t) => {
   .output(path.join(__dirname, 'assets', 'concated_video_test_output.mp4'))
   .concat()
   .then((outputFileName) => {
-    console.log('outputFileName: ', outputFileName);
     t.pass(outputFileName);
+    let input1_duration = shelljs.exec(`ffmpeg -i "${path.join(__dirname, 'assets', 'tailor-5-10.mp4')}" 2>&1 | grep "Duration"| cut -d ' ' -f 4 | sed s/,// | sed 's@\\..*@@g' | awk '{ split($1, A, ":"); split(A[3], B, "."); print 3600*A[1] + 60*A[2] + B[1] }'`, { silent: true }).output;
+    let input2_duration = shelljs.exec(`ffmpeg -i "${path.join(__dirname, 'assets', 'tailor-20-25.mp4')}" 2>&1 | grep "Duration"| cut -d ' ' -f 4 | sed s/,// | sed 's@\\..*@@g' | awk '{ split($1, A, ":"); split(A[3], B, "."); print 3600*A[1] + 60*A[2] + B[1] }'`, { silent: true }).output;
+    let output_duration = shelljs.exec(`ffmpeg -i ${outputFileName} 2>&1 | grep "Duration"| cut -d ' ' -f 4 | sed s/,// | sed 's@\\..*@@g' | awk '{ split($1, A, ":"); split(A[3], B, "."); print 3600*A[1] + 60*A[2] + B[1] }'`, { silent: true }).output;
+    t.equal(parseInt(input1_duration) + parseInt(input2_duration), parseInt(output_duration));
   })
   .catch(err => {
     t.fail(util.inspect(err));
   });
-})
+});
+
+test('Video Stitch Concat Module - spaces in filepath', (t) => {
+  let videoConcat = videoStitch.concat;
+  t.plan(2);
+  videoConcat({
+    silent: false,
+    overwrite: true
+  })
+  .clips([
+    {
+      fileName: path.join(__dirname, 'assets', 'tailor-5-10.mp4'),
+    },
+    {
+      fileName: path.join(__dirname, 'assets', 'tailor-20 to 25.mp4'),
+    }
+  ])
+  .output(path.join(__dirname, 'assets', 'concated_video_test_output.mp4'))
+  .concat()
+  .then((outputFileName) => {
+    t.pass(outputFileName);
+    let input1_duration = shelljs.exec(`ffmpeg -i "${path.join(__dirname, 'assets', 'tailor-5-10.mp4')}" 2>&1 | grep "Duration"| cut -d ' ' -f 4 | sed s/,// | sed 's@\\..*@@g' | awk '{ split($1, A, ":"); split(A[3], B, "."); print 3600*A[1] + 60*A[2] + B[1] }'`, { silent: true }).output;
+    let input2_duration = shelljs.exec(`ffmpeg -i "${path.join(__dirname, 'assets', 'tailor-20 to 25.mp4')}" 2>&1 | grep "Duration"| cut -d ' ' -f 4 | sed s/,// | sed 's@\\..*@@g' | awk '{ split($1, A, ":"); split(A[3], B, "."); print 3600*A[1] + 60*A[2] + B[1] }'`, { silent: true }).output;
+    let output_duration = shelljs.exec(`ffmpeg -i ${outputFileName} 2>&1 | grep "Duration"| cut -d ' ' -f 4 | sed s/,// | sed 's@\\..*@@g' | awk '{ split($1, A, ":"); split(A[3], B, "."); print 3600*A[1] + 60*A[2] + B[1] }'`, { silent: true }).output;
+    t.equal(parseInt(input1_duration) + parseInt(input2_duration), parseInt(output_duration));
+  })
+  .catch(err => {
+    t.fail(util.inspect(err));
+  });
+});
